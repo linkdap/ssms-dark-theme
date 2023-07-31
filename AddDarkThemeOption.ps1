88a5aa2cc03cb32e5baf34faafd450fd776a23ae
Daphne Link <daphne.link@chrobinson.com>
2023-07-31 12:20:32 -0500

    added SSMS version checking and user input
    Script now checks for the users SSMS version that is named in the root directory of the pkgundef file. Script also prompts user to enter the correct directory version that was found before proceeding.

--- AddDarkThemeOption.ps1
+++ AddDarkThemeOption.ps1
@@ -18,7 +18,7 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 else
    {
    # We are not running "as Administrator" - so relaunch as administrator
- 
+   
    # Create a new process object that starts PowerShell
    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
  
@@ -35,11 +35,42 @@ else
    exit
  
    }
-   
-$FileLocation = 'C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms.pkgundef'
+# Indicate to user we are now running as administrator
+
+Write-Host 'Administrator mode activated.'
+#$SSMSVersion = 18 
+$FileLocation = 'C:\Program Files (x86)\Microsoft SQL Server Management Studio ' + $SSMSVersion + '\Common7\IDE\ssms.pkgundef'
 $DarkThemeRemovalKey = '\[\$RootKey\$\\Themes\\{1ded0138-47ce-435e-84ef-9ec1f439b749\}\]'
-$CommentOutDarkThemeRemovalKey = '// [$RootKey$\Themes\{1ded0138-47ce-435e-84ef-9ec1f439b749}]'
-((gc $FileLocation) -replace $DarkThemeRemovalKey, $CommentOutDarkThemeRemovalKey) | sc $FileLocation
-Write-Host 'ssms.pkgundef file successfully changed'
-Write-Host 'SSMS Dark Theme Activated'
-Read-Host -Prompt 'Press Enter to exit'
+$VersionPath = Get-ChildItem -Path "C:\Program Files (x86)\Microsoft SQL Server Management Studio*" -Filter *.pkgundef -Recurse | %{$_.FullName}
+$UserSSMSVersion = $VersionPath.Split('\', 4)[-2].Substring($VersionPath.Split('\', 4)[-2].length - 2, 2)
+$VersionsMatch = $false
+$CommentOutDarkThemeRemovalKey = '// [$RootKey$\Themes\{1ded0138-47ce-435e-84ef-9ec1f439b749}]' 
+
+Write-Host 'Default SSMS Version: '$SSMSVersion
+Write-Host 'Your current SSMS Version: '$UserSSMSVersion
+
+# Check if versions match and update if they do not match 
+while($VersionsMatch -eq $false -And $SSMSVersion -ne $UserSSMSVersion) {
+	Write-Output "`nSSMS version numbers do not match."
+	$SSMSVersion = Read-Host -Prompt 'Enter your version number'
+	
+	if ($SSMSVersion -eq $UserSSMSVersion) {
+		$VersionsMatch = $true
+		# Update filepath based on version number 
+		$FileLocation = 'C:\Program Files (x86)\Microsoft SQL Server Management Studio ' + $SSMSVersion + '\Common7\IDE\ssms.pkgundef'
+	}
+}
+
+# Check if file path exists 
+if (Test-Path -Path $FileLocation) {
+	# Enable dark theme 	
+	((gc $FileLocation) -replace $DarkThemeRemovalKey, $CommentOutDarkThemeRemovalKey) | sc $FileLocation
+	Write-Output "`nssms.pkgundef file successfully changed"
+	Write-Output "SSMS Dark Theme Activated`n"
+	Read-Host -Prompt 'Press Enter to exit'
+	
+} else {
+	# Show user their SSMS path
+	Write-Output "`nDark Theme Activation Failed!!!!"
+	Read-Host -Prompt 'Press Enter to exit'
+}
